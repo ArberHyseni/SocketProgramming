e8cc10853c4a1ffe2889d61e37fe974258c82836
engj3ll <arberpr6@gmail.com>
2020-04-04 23:03:37 +0200

    All

--- Experiment/ClientExperiment.py
+++ Experiment/ClientExperiment.py
@@ -5,7 +5,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
 client.sendall(bytes("Hi",'UTF-8'))
 while True:
-  in_data =  client.recv(1024)
+  in_data = client.recv(1024)
   print("From Server :" ,in_data.decode())
   out_data = input("Command:")
   client.sendall(bytes(out_data,'UTF-8'))
